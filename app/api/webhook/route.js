28c9e4cdeaf19af67a1ce05ecb05c891ee1ad44b
lahcen <hk650110@gmail.com>
2024-02-26 11:07:41 +0100

    route webhook

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,28 +3,38 @@ import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
 
 export async function POST(req) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
 
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error: Missing svix headers", { status: 400 });
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -33,20 +43,36 @@ export async function POST(req) {
     });
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error verifying webhook signature", { status: 400 });
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 
+  // Handle the event
   const eventType = evt?.type;
 
   if (eventType === "user.created" || eventType === "user.updated") {
-    const { id, first_name, last_name, image_url, email_addresses, username } = evt?.data;
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
 
     try {
-      await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses, username);
-      return new Response("User is created or updated", { status: 200 });
+      await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+
+      return new Response("User is created or updated", {
+        status: 200,
+      });
     } catch (err) {
       console.error("Error creating or updating user:", err);
-      return new Response("Error creating or updating user", { status: 500 });
+      return new Response("Error occured", {
+        status: 500,
+      });
     }
   }
 
@@ -54,10 +80,15 @@ export async function POST(req) {
     try {
       const { id } = evt?.data;
       await deleteUser(id);
-      return new Response("User is deleted", { status: 200 });
+
+      return new Response("User is deleted", {
+        status: 200,
+      });
     } catch (err) {
       console.error("Error deleting user:", err);
-      return new Response("Error deleting user", { status: 500 });
+      return new Response("Error occured", {
+        status: 500,
+      });
     }
   }
 }

195a49c215333d34991d1d461c97143a0fc4ffe2
lahcen <hk650110@gmail.com>
2024-02-27 13:18:02 +0100

    middleware

--- middleware.js
+++ middleware.js
@@ -2,11 +2,8 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: ["/api/webhooks"],
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+ 
+  
 });
  
 export const config = {

d57859d0bb5034f08ac27652278328fd8ac5eca5
lahcen <hk650110@gmail.com>
2024-02-28 22:29:28 +0100

    hiehai

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,16 +1,14 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 
 export async function POST(req) {
+
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
   // Get the headers
@@ -21,19 +19,19 @@ export async function POST(req) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.json();
+  const payload = await req.json()
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt;
+  let evt
 
   // Verify the payload with the headers
   try {
@@ -41,21 +39,19 @@ export async function POST(req) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    });
+    })
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
-  // Handle the event
+  // Handle the event 
   const eventType = evt?.type;
 
   if (eventType === "user.created" || eventType === "user.updated") {
-    const { id, first_name, last_name, image_url, email_addresses, username } =
-      evt?.data;
-
+    const { id, first_name, last_name, image_url, email_addresses, username } = evt?.data;
     try {
       await createOrUpdateUser(
         id,
@@ -76,7 +72,6 @@ export async function POST(req) {
       });
     }
   }
-
   if (eventType === "user.deleted") {
     try {
       const { id } = evt?.data;
@@ -92,4 +87,5 @@ export async function POST(req) {
       });
     }
   }
+
 }
--- components/layout/LeftSidebar.jsx
+++ components/layout/LeftSidebar.jsx
@@ -1,40 +1,62 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, UserButton, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, UserButton, SignOutButton , useUser } from "@clerk/nextjs";
 import { Logout } from "@mui/icons-material";
 import Menu from "./Menu";
+import Loader from "@components/Loader";
 
 const LeftSidebar = () => {
-  return (
+  const { user, isLoaded } = useUser();
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+  
+  const getUser = async () => {
+    const response = await fetch(`/api/user/${user.id}`);
+    const data = await response.json();
+    setUserData(data);
+    setLoading(false);
+  };
+  
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, [user]);
+  
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image className="flex-center" src="/logo.svg" alt="logo" width={200} height={200} />
       </Link>
+
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2 items-center text-light-1">
-          <Link href="/">
+            <Link href={"/"}>
             <Image
-              src="/profile1 (2).jpeg"
-              alt="profile"
-              width={70}
-              height={70}
+              src={userData?.profilePhoto}
+              alt="profilephoto"
+              width={50}
+              height={50}
               className="rounded-full"
             />
           </Link>
-          <p className="text-small-bold">Pedro alonso</p>
+          <p className="text-small-bold">{userData?.firstName} {userData?.lastName}</p>
         </div>
         <div className="flex text-light-1 justify-between">
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">1</p>
+            <p className="text-base-bold">{userData?.posts?.length}</p>
             <p className="text-tiny-medium">Posts</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">1</p>
+            <p className="text-base-bold">{userData?.followers?.length}</p>
             <p className="text-tiny-medium">Posts</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-base-bold">1</p>
+            <p className="text-base-bold">{userData?.following?.length}</p>
             <p className="text-tiny-medium">Posts</p>
           </div>
         </div>
